f87286e7f8a1c73abc76770f9e501505fe6ea1a4
EkoEdyPurwanto <ekoedypurwantooke@gmail.com>
2024-05-12 01:19:27 +0700

    feat: declare up and down db migrations

--- db/migrations/20240511172535_goforit.up.sql
+++ db/migrations/20240511172535_goforit.up.sql
@@ -1,4 +1,10 @@
 CREATE TABLE IF NOT EXISTS "user_credential"
 (
-
+    -- column
+    id         VARCHAR PRIMARY KEY NOT NULL,
+    username   VARCHAR UNIQUE      NOT NULL,
+    email      VARCHAR UNIQUE      NOT NULL,
+    created_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMPTZ,
+    deleted_at TIMESTAMPTZ
 );
