846277ac39e6f4a56c45afd821406473d6750ca4
EkoEdyPurwanto <ekoedypurwantooke@gmail.com>
2024-06-05 22:02:58 +0700

    clarify: structure of db

--- db/migrations/20240511172535_goforit.up.sql
+++ db/migrations/20240511172535_goforit.up.sql
@@ -2,8 +2,9 @@ CREATE TABLE IF NOT EXISTS "user_credential"
 (
     -- column
     id         VARCHAR PRIMARY KEY NOT NULL,
-    username   VARCHAR UNIQUE      NOT NULL,
-    email      VARCHAR UNIQUE      NOT NULL,
+    username   VARCHAR(15) UNIQUE  NOT NULL,
+    email      VARCHAR(50) UNIQUE  NOT NULL,
+    password   VARCHAR(100)        NOT NULL,
     created_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMPTZ,
     deleted_at TIMESTAMPTZ
