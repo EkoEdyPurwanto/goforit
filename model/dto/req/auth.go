c14721b792eaeb29d1b55b8163238d892674c4e0
EkoEdyPurwanto <ekoedypurwantooke@gmail.com>
2024-06-05 22:18:57 +0700

    chore: validation update

--- model/dto/req/auth.go
+++ model/dto/req/auth.go
@@ -2,7 +2,7 @@ package req
 
 type (
 	AuthRegisterRequest struct {
-		Username        string `json:"username" validate:"required,min=3,max=10"`
+		Username        string `json:"username" validate:"required,min=3,max=15"`
 		Email           string `json:"email" validate:"required,email"`
 		Password        string `json:"password" validate:"required,min=6,max=20"`
 		PasswordConfirm string `json:"password_confirm" validate:"required,eqfield=Password"`
